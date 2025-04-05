908c0fb40c4d12d3b05f00d65d874633b1d74056
CelineSelke1 <celineselke@outlook.com>
2025-04-05 13:55:01 -0400

    add question data parsing

---
+++ common.php
@@ -0,0 +1,46 @@
+<?php
+function get4Answers(){
+    $csvFile = './questions/questions/questions_4_answers.csv';
+
+    // Check if the file exists
+    if (!file_exists($csvFile)) {
+        exit('File not found.');
+    }
+
+    $file = fopen($csvFile, 'r');
+
+    // Read the entire file into an array of rows
+    $rows = [];
+    while (($row = fgetcsv($file)) !== false) {
+        $rows[] = $row;
+    }
+
+    fclose($file);
+
+    // Check if there are any rows in the CSV
+    if (empty($rows)) {
+        die('CSV file is empty.');
+    }
+
+    // select a row at random
+    $randomRow = $rows[array_rand($rows)];
+
+    // Assign values to the array with the required keys
+    $quizData = [
+        'question'        => $randomRow[0],    
+        'answer1'         => $randomRow[1],    
+        'answer1points'   => $randomRow[2],   
+        'answer2'         => $randomRow[3],    
+        'answer2points'   => $randomRow[4],    
+        'answer3'         => $randomRow[5],   
+        'answer3points'   => $randomRow[6],    
+        'answer4'         => $randomRow[7],    
+        'answer4points'   => $randomRow[8],   
+    ];
+
+    return $quizData;
+
+}
+
+
+?>
---
+++ round1.php
@@ -0,0 +1,19 @@
+<?php
+    include "common.php";
+
+    $QandA = get4Answers();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Family Feud</title>
+</head>
+<body>
+    
+    
+</body>
+</html>
