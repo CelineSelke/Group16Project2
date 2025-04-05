215bc1e042aedbc5ad4e33d7926f2f90c8e55ac1
CelineSelke1 <celineselke@outlook.com>
2025-04-05 13:43:22 -0400

    create homepage, add question data

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-<?php
-
-?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Family Feud</title>
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+    <div id="logo-container">
+        <img id="logo" src="./images/logo.png" alt="Family Feud Logo">
+    </div>
+    <div id="button-row">
+        <a href="round1.php"><button id="play-button"><img src="./images/play.png" alt="Play Button"></button></a>
+        <a href="leaderboard.php"><button id="leaderboard-button"><img src="./images/leaderboard.webp" alt="Leaderboard Button"></button></a>
+    </div>
+</body>
+</html>
