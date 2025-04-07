31d9ed9e9ef12d2a4f5a5a6214fd975e9acb49c9
ThomasNVU <thomasvu.tech@gmail.com>
2025-04-07 09:45:33 -0400

    Change Title Screen

--- index.php
+++ index.php
@@ -17,12 +17,14 @@
     <link rel="stylesheet" href="./css/styles.css">
 </head>
 <body>
-    <div id="logo-container">
-        <img id="logo" src="./images/logo.png" alt="Family Feud Logo">
-    </div>
-    <div id="button-row">
-        <a href="round1.php"><button id="play-button"><img src="./images/play.png" alt="Play Button"></button></a>
-        <a href="leaderboard.php"><button id="leaderboard-button"><img src="./images/leaderboard.webp" alt="Leaderboard Button"></button></a>
+    <div id="oval">
+        <div id="logo-container">
+            <img id="logo" src="./images/logo.png" alt="Family Feud Logo">
+        </div>
+        <div id="button-row">
+            <a href="round1.php"><button id="play-button"><img src="./images/play.png" alt="Play Button"></button></a>
+            <a href="leaderboard.php"><button id="leaderboard-button"><img src="./images/leaderboard.webp" alt="Leaderboard Button"></button></a>
+        </div>
     </div>
 </body>
 </html>
