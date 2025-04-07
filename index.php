cba65fb1beda3dd2bb5a3beed4a70be70ed9a689
ThomasNVU <thomasvu.tech@gmail.com>
2025-04-07 16:52:06 -0400

    Correct index.php to round.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
             <img id="logo" src="./images/logo.png" alt="Family Feud Logo">
         </div>
         <div id="button-row">
-            <a href="round1.php"><button id="play-button"><img src="./images/play.png" alt="Play Button"></button></a>
+            <a href="round.php"><button id="play-button"><img src="./images/play.png" alt="Play Button"></button></a>
             <a href="leaderboard.php"><button id="leaderboard-button"><img src="./images/leaderboard.webp" alt="Leaderboard Button"></button></a>
         </div>
     </div>
